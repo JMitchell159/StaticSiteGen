85b3dd59070dbf2ae63601efe1bd7c2c7615f008
JMichell159 <joemitchell159@gmail.com>
2025-06-27 06:59:21 -0500

    added link and image handling as well as unit tests for it

---
+++ src/test_image_and_link_handling.py
@@ -0,0 +1,21 @@
+import unittest
+
+from image_and_link_handling import extract_markdown_images, extract_markdown_links
+
+class TestImageAndLinkHandling(unittest.TestCase):
+    def test1(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertListEqual(extract_markdown_images(text), result)
+    
+    def test2(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertListEqual(extract_markdown_links(text), result)
+    
+    def test3(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+if __name__ == "__main__":
+    unittest.main()
