de361071ca824ca47116b571aa7849cf4103e1e4
JMichell159 <joemitchell159@gmail.com>
2025-07-03 06:22:08 -0500

    main now copies files from static to public folder

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 import text_to_html
+import os
+import shutil
 
 def main():
-    text_node = TextNode("funny haha", TextType.PLAIN_TEXT.value)
-    print(text_node)
+    workspace_path = "/home/joemi/workspace/github.com/JMitchell159/StaticSiteGen"
+    shutil.rmtree(f"{workspace_path}/public")
+    shutil.copytree(f"{workspace_path}/static", f"{workspace_path}/public")
 
-main()
+
+if __name__ == "__main__":
+    main()
